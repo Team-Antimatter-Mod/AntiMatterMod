c1ff92ed4e493044c00df9794b87e2a6a60f7633
C6H2Cl2 <ma87kk26ml@gmail.com>
2016-09-19 22:23:17 +0900

    機械用のInterfaceの整備

--- src/main/java/antimattaermod/core/Energy/IAPGenerator.java
+++ src/main/java/antimattaermod/core/Energy/IAPGenerator.java
@@ -3,5 +3,6 @@ package antimattaermod.core.Energy;
 /**
  * @author C6H2Cl2
  */
-public interface IAPGenerator {
+public interface IAPGenerator extends IAPMachine{
+
 }
--- src/main/java/antimattaermod/core/Energy/IAPMachine.java
+++ src/main/java/antimattaermod/core/Energy/IAPMachine.java
@@ -3,6 +3,12 @@ package antimattaermod.core.Energy;
 /**
  * @author C6H2Cl2
  */
+
 public interface IAPMachine {
     int getMaxStoreEnergy();
+    boolean canSendEnergy();
+    boolean canReciveEnergy();
+    APVoltage getSendVoltage();
+    APVoltage getReciveVoltage();
+
 }
