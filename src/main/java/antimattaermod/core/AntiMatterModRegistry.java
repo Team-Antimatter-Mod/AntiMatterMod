0273cdcf8ba34ebbb30f5a2db0cc1fea3301d991
Raiti <rusuke@t-net.ne.jp>
2016-09-20 01:28:44 +0900

    クリエイティブタブが先に書かれてないと正しくタブに追加されないので上にした

--- src/main/java/antimattaermod/core/AntiMatterModRegistry.java
+++ src/main/java/antimattaermod/core/AntiMatterModRegistry.java
@@ -25,26 +25,7 @@ import java.util.List;
  * @author C6H2Cl2
  */
 public class AntiMatterModRegistry {
-    //Item  ============================================================================================================
-    //素材
-    public static Item materials = ItemUtil.CreateItem(new MetaItemBase(13) {
-        @Override
-        public void addInformation(ItemStack item, EntityPlayer player, List list, boolean p_77624_4_) {
-            if(item.getItemDamage() == 11) {
-                list.add(StatCollector.translateToLocal("tile.material_11.information_1"));
-                list.add(StatCollector.translateToLocal("tile.material_11.information_2"));
-                list.add(ChatFormatting.RED+StatCollector.translateToLocal("tile.material_11.information.name"));
-            }
-        }
-    },"material", "antimattermodcore:material", AntiMatterModRegistry.tabMaterials);
-    //==================================================================================================================
-
-    //Block  ===========================================================================================================
-    //鉱石
-    public static Block crystalOreBlock_1 = new CrystalOreBlock(Material.rock, "crystalOreBlock_1", "antimattermodcore:crystaloreblock_1", AntiMatterModRegistry.tabOreBlock, 2,
-            new float[]{5.0F,5.0F}, new byte[]{3,3}, materials, new int[]{10,12});
-    //==================================================================================================================
-
+	
     //CreativeTab ======================================================================================================
     //鉱石
     public static final CreativeTabs tabOreBlock = new CreativeTabs("tabOreBlock") {
@@ -70,6 +51,28 @@ public class AntiMatterModRegistry {
     };
 
     //==================================================================================================================
+	
+    //Item  ============================================================================================================
+    //素材
+    public static Item materials = ItemUtil.CreateItem(new MetaItemBase(13) {
+        @Override
+        public void addInformation(ItemStack item, EntityPlayer player, List list, boolean p_77624_4_) {
+            if(item.getItemDamage() == 11) {
+                list.add(StatCollector.translateToLocal("tile.material_11.information_1"));
+                list.add(StatCollector.translateToLocal("tile.material_11.information_2"));
+                list.add(ChatFormatting.RED+StatCollector.translateToLocal("tile.material_11.information.name"));
+            }
+        }
+    },"material", "antimattermodcore:material", AntiMatterModRegistry.tabMaterials);
+    //==================================================================================================================
+
+    //Block  ===========================================================================================================
+    //鉱石
+    public static Block crystalOreBlock_1 = new CrystalOreBlock(Material.rock, "crystalOreBlock_1", "antimattermodcore:crystaloreblock_1", AntiMatterModRegistry.tabOreBlock, 2,
+            new float[]{5.0F,5.0F}, new byte[]{3,3}, materials, new int[]{10,12});
+    //==================================================================================================================
+
+
 
     //preinitで行う登録処理
     public static void registerPreInit(FMLPreInitializationEvent event){
