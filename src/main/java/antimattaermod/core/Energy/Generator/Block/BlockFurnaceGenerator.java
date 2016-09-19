0c0f216d56da602e7529973763fcb4650f2e5691
C6H2Cl2 <ma87kk26ml@gmail.com>
2016-09-19 22:27:24 +0900

    最低限の実装

--- src/main/java/antimattaermod/core/Energy/Generator/Block/BlockFurnaceGenerator.java
+++ src/main/java/antimattaermod/core/Energy/Generator/Block/BlockFurnaceGenerator.java
@@ -1,7 +1,21 @@
 package antimattaermod.core.Energy.Generator.Block;
 
+import antimattaermod.core.Energy.Generator.TileEntity.TileEntityFurnaceGenerator;
+import net.minecraft.block.BlockContainer;
+import net.minecraft.block.material.Material;
+import net.minecraft.tileentity.TileEntity;
+import net.minecraft.world.World;
+
 /**
  * @author C6H2Cl2
  */
-public class BlockFurnaceGenerator {
+public class BlockFurnaceGenerator extends BlockContainer {
+    protected BlockFurnaceGenerator() {
+        super(Material.rock);
+    }
+
+    @Override
+    public TileEntity createNewTileEntity(World p_149915_1_, int p_149915_2_) {
+        return new TileEntityFurnaceGenerator();
+    }
 }
--- src/main/java/antimattaermod/core/Energy/Generator/TileEntity/TileEntityFurnaceGenerator.java
+++ src/main/java/antimattaermod/core/Energy/Generator/TileEntity/TileEntityFurnaceGenerator.java
@@ -1,7 +1,10 @@
 package antimattaermod.core.Energy.Generator.TileEntity;
 
+import net.minecraft.tileentity.TileEntity;
+
 /**
  * @author C6H2Cl2
  */
-public class TileEntityFurnaceGenerator {
+public class TileEntityFurnaceGenerator extends TileEntity{
+
 }
