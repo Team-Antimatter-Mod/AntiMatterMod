7c678e9d46f6060a82777a6a241dc772e9ffeb90
C6H2Cl2 <ma87kk26ml@gmail.com>
2016-09-21 00:05:55 +0900

    エネルギー生産処理の実装

--- src/main/java/antimattaermod/core/Energy/Generator/TileEntity/TileEntityFurnaceGenerator.kt
+++ src/main/java/antimattaermod/core/Energy/Generator/TileEntity/TileEntityFurnaceGenerator.kt
@@ -2,14 +2,17 @@ package antimattaermod.core.Energy.Generator.TileEntity
 
 import antimattaermod.core.Energy.APVoltage
 import antimattaermod.core.Energy.IAPGenerator
+import net.minecraft.nbt.NBTTagCompound
+import net.minecraft.network.NetworkManager
+import net.minecraft.network.Packet
+import net.minecraft.network.play.server.S25PacketBlockBreakAnim
+import net.minecraft.network.play.server.S35PacketUpdateTileEntity
 import net.minecraft.tileentity.TileEntity
 
 /**
  * @author C6H2Cl2
  */
 class TileEntityFurnaceGenerator : TileEntity(),IAPGenerator{
-
-
 
     //定数
     private val voltage : APVoltage = APVoltage.HV
@@ -18,10 +21,42 @@ class TileEntityFurnaceGenerator : TileEntity(),IAPGenerator{
     //変数
     private var storedEnergy : Int = 0
     private var fuel : Float = 0f
+    private var currentGenerate : Int = 0
     init {
 
     }
 
+    //NBTに書き込み
+    override fun writeToNBT(tagCompound: NBTTagCompound?) {
+        super.writeToNBT(tagCompound)
+        tagCompound!!.setInteger("storedEnergy",storedEnergy)
+        tagCompound.setFloat("fuel",fuel)
+    }
+
+    //NBTから読み出し
+    override fun readFromNBT(tagCompound: NBTTagCompound?) {
+        super.readFromNBT(tagCompound)
+        storedEnergy = tagCompound!!.getInteger("storedEnergy")
+        fuel = tagCompound.getFloat("fuel")
+    }
+
+    //tickごとの処理
+    override fun updateEntity() {
+        super.updateEntity()
+        if(fuel > 1f){
+            currentGenerate = voltage.maxEnergy
+            storedEnergy += voltage.maxEnergy
+            fuel -= 1f
+        }else if (fuel > 0f){
+            currentGenerate = (voltage.maxEnergy * fuel).toInt()
+            storedEnergy += currentGenerate
+            fuel = 0f
+        }else{
+            currentGenerate = 0
+        }
+    }
+
+    //燃料追加
     public fun addFuel(amount: Float) :Float{
         val value :Float= amount / 1600
         fuel += value
@@ -34,6 +69,16 @@ class TileEntityFurnaceGenerator : TileEntity(),IAPGenerator{
         }
     }
 
+    //データの同期
+    override fun onDataPacket(net: NetworkManager?, pkt: S35PacketUpdateTileEntity?) {
+        readFromNBT(pkt!!.func_148857_g())
+    }
+
+    override fun getDescriptionPacket(): Packet {
+        val tagCompound = NBTTagCompound()
+        writeToNBT(tagCompound)
+        return S35PacketUpdateTileEntity(xCoord,yCoord,zCoord,1,tagCompound)
+    }
 
     override fun getStoredEnergy(): Int {
         return storedEnergy
@@ -64,7 +109,7 @@ class TileEntityFurnaceGenerator : TileEntity(),IAPGenerator{
     }
 
     override fun getCurrentGenerate(): Int {
-        return 0
+        return currentGenerate
     }
 
     override fun getFuelValue(): Float {
