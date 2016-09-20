66a98844c008d69d69d7c09a8a3dcff498755aa2
C6H2Cl2 <ma87kk26ml@gmail.com>
2016-09-20 09:24:33 +0900

    こっとり～ん

---
+++ src/main/java/antimattaermod/core/Energy/Generator/TileEntity/TileEntityFurnaceGenerator.kt
@@ -0,0 +1,12 @@
+package antimattaermod.core.Energy.Generator.TileEntity
+
+import net.minecraft.tileentity.TileEntity
+
+/**
+ * @author C6H2Cl2
+ */
+class TileEntityFurnaceGenerator : TileEntity(){
+    init {
+
+    }
+}
