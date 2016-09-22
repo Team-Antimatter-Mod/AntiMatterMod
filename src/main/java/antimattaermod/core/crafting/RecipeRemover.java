ad9460f25be595795990bfbde595089250c7ac36
Raiti <rusuke@t-net.ne.jp>
2016-09-22 12:58:42 +0900

    アイテムを指定してレシピを消去するメソッドを追加

--- src/main/java/antimattaermod/core/crafting/RecipeRemover.java
+++ src/main/java/antimattaermod/core/crafting/RecipeRemover.java
@@ -1,6 +1,7 @@
 package antimattaermod.core.crafting;
 
 import net.minecraft.init.Items;
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.crafting.CraftingManager;
 import net.minecraft.item.crafting.IRecipe;
@@ -24,4 +25,42 @@ public class RecipeRemover {
             }
         }
     }
+
+    /**
+     * 指定したアイテムのレシピを消去します。<br>
+     * 複数レシピがあった場合すべて消去します。<br>
+     * メタデータを持ったアイテムのレシピを利用する場合{@link RecipeRemover#removeRecipe(Item, int)}を利用してください。
+     * @param item 消去するアイテム
+     */
+    public static void removeRecipe(Item item){
+        List<IRecipe> recipes = CraftingManager.getInstance().getRecipeList();//全レシピを取得
+        Iterator<IRecipe> remover = recipes.iterator();//レシピリストのイテレーター取得
+
+        while (remover.hasNext()){
+            ItemStack outItem = remover.next().getRecipeOutput();
+            if(outItem != null && outItem.getItem() == item){
+                remover.remove();
+            }
+        }
+
+    }
+
+    /**
+     * 指定したアイテム、メタデータのレシピを消去します。<br>
+     * 複数レシピがあった場合すべて消去します。
+     * @param item 消去するレシピのアイテム
+     * @param meta 消去するレシピのアイテムのメタデータ
+     */
+    public  static  void removeRecipe(Item item,int meta){
+        List<IRecipe> recipes = CraftingManager.getInstance().getRecipeList();//全レシピを取得
+        Iterator<IRecipe> remover = recipes.iterator();//レシピリストのイテレーター取得
+
+        while (remover.hasNext()){
+            ItemStack outItem = remover.next().getRecipeOutput();
+            if(outItem != null && outItem.getItem() == item && outItem.getItemDamage() == meta){
+                remover.remove();
+            }
+        }
+    }
+
 }
