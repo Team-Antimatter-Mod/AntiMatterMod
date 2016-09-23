828b025509793db6fc598ecb0c8862ffdbb2ccf0
C6H2Cl2 <ma87kk26ml@gmail.com>
2016-09-23 20:30:50 +0900

    ケーブルのNBTでのW/R処理

--- src/main/java/antimattaermod/core/Energy/Transfer/TileEntityCable.kt
+++ src/main/java/antimattaermod/core/Energy/Transfer/TileEntityCable.kt
@@ -1,5 +1,6 @@
 package antimattaermod.core.Energy.Transfer
 
+import net.minecraft.nbt.NBTTagCompound
 import net.minecraft.tileentity.TileEntity
 import net.minecraftforge.common.util.ForgeDirection
 import java.util.*
@@ -16,6 +17,29 @@ class TileEntityCable :TileEntity(){
         isConnected[ForgeDirection.WEST] = false
         isConnected[ForgeDirection.NORTH] = false
         isConnected[ForgeDirection.SOUTH] = false
+    }
+
+    override fun writeToNBT(nbtTagCompound: NBTTagCompound?) {
+        super.writeToNBT(nbtTagCompound)
+        val tagCompound :NBTTagCompound = NBTTagCompound()
+        tagCompound.setBoolean("Up",isConnected[ForgeDirection.UP] as Boolean)
+        tagCompound.setBoolean("Down",isConnected[ForgeDirection.DOWN] as Boolean)
+        tagCompound.setBoolean("East",isConnected[ForgeDirection.EAST] as Boolean)
+        tagCompound.setBoolean("West",isConnected[ForgeDirection.WEST] as Boolean)
+        tagCompound.setBoolean("North",isConnected[ForgeDirection.NORTH] as Boolean)
+        tagCompound.setBoolean("South",isConnected[ForgeDirection.SOUTH] as Boolean)
+        nbtTagCompound!!.setTag("Connection",tagCompound)
+    }
+
+    override fun readFromNBT(nbtTagCompound: NBTTagCompound?) {
+        super.readFromNBT(nbtTagCompound)
+        val tagCompound : NBTTagCompound = nbtTagCompound!!.getCompoundTag("Connection")
+        isConnected[ForgeDirection.UP] = tagCompound.getBoolean("Up")
+        isConnected[ForgeDirection.DOWN] = tagCompound.getBoolean("Down")
+        isConnected[ForgeDirection.EAST] = tagCompound.getBoolean("East")
+        isConnected[ForgeDirection.WEST] = tagCompound.getBoolean("West")
+        isConnected[ForgeDirection.NORTH] = tagCompound.getBoolean("North")
+        isConnected[ForgeDirection.SOUTH] = tagCompound.getBoolean("South")
     }
 
     public fun setConnection(direction : ForgeDirection, isConnect : Boolean) {
