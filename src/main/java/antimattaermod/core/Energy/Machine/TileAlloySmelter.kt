30db253596115e1754c30aa912ce8e43dd6846f6
C6H2Cl2 <ma87kk26ml@gmail.com>
2016-10-13 00:21:47 +0900

    IInventoryの実装

--- src/main/java/antimattaermod/core/Energy/Machine/TileAlloySmelter.kt
+++ src/main/java/antimattaermod/core/Energy/Machine/TileAlloySmelter.kt
@@ -1,5 +1,9 @@
 package antimattaermod.core.Energy.Machine
 
+import net.minecraft.entity.player.EntityPlayer
+import net.minecraft.inventory.IInventory
+import net.minecraft.inventory.ISidedInventory
+import net.minecraft.item.ItemStack
 import net.minecraft.nbt.NBTTagCompound
 import net.minecraft.network.NetworkManager
 import net.minecraft.network.Packet
@@ -9,14 +13,17 @@ import net.minecraft.tileentity.TileEntity
 /**
  * @author C6H2Cl2
  */
-class TileAlloySmelter(private var tier:Int) :TileEntity(){
+class TileAlloySmelter(private var tier:Int) :TileEntity(),IInventory{
+
     private var slotSize = 0
+    private var items : Array<ItemStack?> = emptyArray()
     init {
         when(tier){
             1 -> slotSize = 2
             2 -> slotSize = 4
             3 -> slotSize = 9
         }
+        items = kotlin.arrayOfNulls(slotSize)
     }
 
     public fun getTier():Int = tier
@@ -49,5 +56,56 @@ class TileAlloySmelter(private var tier:Int) :TileEntity(){
         val tagCompound = NBTTagCompound()
         writeToNBT(tagCompound)
         return S35PacketUpdateTileEntity(xCoord,yCoord,zCoord,1,tagCompound)
+    }
+
+    override fun setInventorySlotContents(p_70299_1_: Int, p_70299_2_: ItemStack?) {
+        throw UnsupportedOperationException("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun closeInventory() {
+        //ToDo: Inventoryを閉じたときの処理
+    }
+
+    override fun isUseableByPlayer(p_70300_1_: EntityPlayer?): Boolean {
+        return true
+    }
+
+    override fun getInventoryName(): String {
+        return "alloySmelterTier"+tier
+    }
+
+    override fun isItemValidForSlot(p_94041_1_: Int, p_94041_2_: ItemStack?): Boolean {
+        return true
+    }
+
+    override fun hasCustomInventoryName(): Boolean {
+        return false
+    }
+
+    override fun getSizeInventory(): Int {
+        return slotSize
+    }
+
+    override fun decrStackSize(p_70298_1_: Int, p_70298_2_: Int): ItemStack {
+        //ToDo: ???
+        return items[p_70298_1_] as ItemStack
+    }
+
+    override fun openInventory() {
+        //ToDo: Inventoryを開いたときの処理
+    }
+
+    override fun getInventoryStackLimit(): Int {
+        return 64
+    }
+
+    override fun getStackInSlotOnClosing(p_70304_1_: Int): ItemStack? {
+        //ToDO: Slot内のItemStackを返す
+        return items[p_70304_1_]
+    }
+
+    override fun getStackInSlot(p_70301_1_: Int): ItemStack? {
+        //ToDO: Slot内のItemStackを返す
+        return items[p_70301_1_]
     }
 }
