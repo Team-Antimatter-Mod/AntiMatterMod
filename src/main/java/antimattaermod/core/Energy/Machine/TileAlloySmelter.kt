4c57f78cef8cfb1e50647e34564629b3293d923e
C6H2Cl2 <ma87kk26ml@gmail.com>
2016-10-13 21:13:17 +0900

    IInventoryの実装完了

--- src/main/java/antimattaermod/core/Energy/Machine/TileAlloySmelter.kt
+++ src/main/java/antimattaermod/core/Energy/Machine/TileAlloySmelter.kt
@@ -2,7 +2,6 @@ package antimattaermod.core.Energy.Machine
 
 import net.minecraft.entity.player.EntityPlayer
 import net.minecraft.inventory.IInventory
-import net.minecraft.inventory.ISidedInventory
 import net.minecraft.item.ItemStack
 import net.minecraft.nbt.NBTTagCompound
 import net.minecraft.network.NetworkManager
@@ -16,23 +15,31 @@ import net.minecraft.tileentity.TileEntity
 class TileAlloySmelter(private var tier:Int) :TileEntity(),IInventory{
 
     private var slotSize = 0
-    private var items : Array<ItemStack?> = emptyArray()
+    /*Slotの数について
+    SlotNumは、材料のSlotの数とする
+    なので、IInventoryからの取得分は成果物のスロット数2を足して返す
+    なお、スロット0～slotSize-1は材料、slotSize～slotSize+2は成果物
+     */
+    private var materialSlotItem: Array<ItemStack?> = emptyArray()
+    private var resultSlotItem: Array<ItemStack?> = kotlin.arrayOfNulls(2)
     init {
         when(tier){
             1 -> slotSize = 2
             2 -> slotSize = 4
             3 -> slotSize = 9
         }
-        items = kotlin.arrayOfNulls(slotSize)
+        materialSlotItem = kotlin.arrayOfNulls(slotSize)
     }
 
     public fun getTier():Int = tier
     public fun getSlotSize():Int = slotSize
 
+    //tickごとの処理
     override fun updateEntity() {
         super.updateEntity()
     }
 
+    //NBTに書き込みしてデータ保存
     override fun writeToNBT(tagCompound: NBTTagCompound) {
         super.writeToNBT(tagCompound)
         val tag = NBTTagCompound()
@@ -41,6 +48,7 @@ class TileAlloySmelter(private var tier:Int) :TileEntity(),IInventory{
         tagCompound.setTag("data",tag)
     }
 
+    //NBTからデータ読み込み
     override fun readFromNBT(tagCompound: NBTTagCompound) {
         super.readFromNBT(tagCompound)
         val tag = tagCompound.getCompoundTag("data")
@@ -48,22 +56,28 @@ class TileAlloySmelter(private var tier:Int) :TileEntity(),IInventory{
         slotSize = tag.getInteger("slotSize")
     }
 
+    //同期用のパケット読み込み
     override fun onDataPacket(net: NetworkManager?, pkt: S35PacketUpdateTileEntity?) {
         readFromNBT(pkt!!.func_148857_g())
     }
 
+    //同期用のパケット返す
     override fun getDescriptionPacket(): Packet {
         val tagCompound = NBTTagCompound()
         writeToNBT(tagCompound)
         return S35PacketUpdateTileEntity(xCoord,yCoord,zCoord,1,tagCompound)
     }
 
-    override fun setInventorySlotContents(p_70299_1_: Int, p_70299_2_: ItemStack?) {
-        throw UnsupportedOperationException("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
-
-    override fun closeInventory() {
-        //ToDo: Inventoryを閉じたときの処理
+    //slotNumberのSlotに、ItemStack突っ込むだけ
+    override fun setInventorySlotContents(slotNumber: Int, itemStack: ItemStack?) {
+        if (itemStack != null && itemStack.stackSize > inventoryStackLimit){
+            itemStack.stackSize = inventoryStackLimit
+        }
+        if (slotNumber >= slotSize) {
+            materialSlotItem[slotNumber] = itemStack
+        }else{
+            resultSlotItem[slotNumber-slotSize] = itemStack
+        }
     }
 
     override fun isUseableByPlayer(p_70300_1_: EntityPlayer?): Boolean {
@@ -83,29 +97,51 @@ class TileAlloySmelter(private var tier:Int) :TileEntity(),IInventory{
     }
 
     override fun getSizeInventory(): Int {
-        return slotSize
+        return slotSize + 2
     }
 
-    override fun decrStackSize(p_70298_1_: Int, p_70298_2_: Int): ItemStack {
-        //ToDo: ???
-        return items[p_70298_1_] as ItemStack
+    //第一引数のスロットのItemStackのStack数を最大で第二引数個減らし、減らした分のItemStackを返す
+    override fun decrStackSize(slotNumber: Int, stackSize: Int): ItemStack? {
+        if (materialSlotItem[slotNumber] != null){
+            if(materialSlotItem[slotNumber]!!.stackSize <= stackSize){
+                val itemStack = materialSlotItem[slotNumber]
+                materialSlotItem[slotNumber] = null
+                markDirty()
+                return itemStack
+            }else{
+                val itemStack = materialSlotItem[slotNumber]!!.splitStack(stackSize)
+                markDirty()
+                return itemStack
+            }
+        }else{
+            return null
+        }
     }
 
     override fun openInventory() {
-        //ToDo: Inventoryを開いたときの処理
+        //何もなしでおｋ
     }
 
+    override fun closeInventory() {
+        //何もなしでおｋ
+    }
+
+    //InventoryのSlotごとのスタック数の上限
     override fun getInventoryStackLimit(): Int {
         return 64
     }
 
-    override fun getStackInSlotOnClosing(p_70304_1_: Int): ItemStack? {
-        //ToDO: Slot内のItemStackを返す
-        return items[p_70304_1_]
+    //そのまま返せばおｋ
+    override fun getStackInSlotOnClosing(slotNumber: Int): ItemStack? {
+        return getStackInSlot(slotNumber)
     }
 
-    override fun getStackInSlot(p_70301_1_: Int): ItemStack? {
-        //ToDO: Slot内のItemStackを返す
-        return items[p_70301_1_]
+    //そのまま返せばおｋ
+    override fun getStackInSlot(slotNumber: Int): ItemStack? {
+        return if (slotNumber >= slotSize) {
+            materialSlotItem[slotNumber]
+        }else{
+            resultSlotItem[slotNumber-slotSize]
+        }
     }
 }
