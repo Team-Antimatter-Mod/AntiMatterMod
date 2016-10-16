f05aaa7e479e3e69c85b7235b12798bebd3026e2
Raiti <rusuke@t-net.ne.jp>
2016-10-16 20:59:35 +0900

    ドライバークラスの追加

---
+++ src/main/java/antimattaermod/core/Item/tool/ScrewDriver.java
@@ -0,0 +1,19 @@
+package antimattaermod.core.Item.tool;
+
+import org.jetbrains.annotations.NotNull;
+
+/**
+ * Created by Raiti-chan on 2016/10/16.
+ *
+ * @author Raiti-chan
+ */
+public class ScrewDriver extends CraftingTool {
+	
+	public ScrewDriver(@NotNull String name, @NotNull String textureName, ToolMaterial material) {
+		super(name, textureName, material);
+	}
+	
+	public ScrewDriver(@NotNull String name, @NotNull String textureName, int maxUses) {
+		super(name, textureName, maxUses);
+	}
+}
