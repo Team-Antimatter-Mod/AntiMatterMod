fbe2ecda11e1a8533d00b4ed5bccc94b1d7c5601
Raiti <rusuke@t-net.ne.jp>
2016-12-28 23:50:29 +0900

    ファイルダウンロードのメソッド追加

---
+++ src/main/java/antimattermod/core/fileIO/AMMFiles.java
@@ -0,0 +1,22 @@
+package antimattermod.core.fileIO;
+
+
+/**
+ * AMMの各ファイルパスの保管場所？<br>
+ * もしかすると実装を追加するかも
+ * <br>Created by Raiti-chan on 2016/12/28.
+ *
+ * @author Raiti-chan
+ * @version 1.0.0
+ * @since 1.0.0
+ */
+@SuppressWarnings("unused")
+public class AMMFiles {
+    
+    /**
+     * このMODのConfig用のパス
+     */
+    public static final String CONFIG_DIRECTORY = "config/AntiMatterMod/";
+    
+    
+}
